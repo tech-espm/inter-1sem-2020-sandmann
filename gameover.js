ef7a7cdc9471dea3649cc2ca1efe3344fc99e0e6
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-06-19 16:51:05 -0300

    Add files via upload

---
+++ gameover.js
@@ -0,0 +1,56 @@
+﻿function gameover() {
+	
+	var scoretext;
+	
+	this.preload = function () {
+	game.load.image("gameover", "gameover.png");
+	game.load.image("retry", "retry.png");
+	game.load.image("title", "title.png");
+	game.load.audio("lose6", "lose6.mp3");
+	};
+	
+	this.create = function () {
+		
+			var estilo = {
+			font: "bold 68px OCR A",
+			fill: "#FFFFFF"
+		};
+		
+		musica.destroy();
+		lose6 = game.add.audio("lose6" , 3);
+		lose6.play();
+		
+	//fundo
+	game.add.tileSprite(0, 0, 800,640, "gameover");
+	//configurarretry
+	retry = game.add.image(50, 520, "retry");
+	retry.inputEnabled = true;
+	retry.input.useHandCursor = true;
+	retry.events.onInputDown.add(retryFoiClicado);
+	//configurartitle
+	title = game.add.image(510, 520, "title");
+	title.inputEnabled = true;
+	title.input.useHandCursor = true;
+	title.events.onInputDown.add(titleFoiClicado);
+	
+		scoretxt = game.add.text(96, 150, "FINAL SCORE:"+pontuacao, estilo);
+	fadeIn();
+		};
+
+//clicarnoretry
+	function retryFoiClicado() {
+		fadeOut(fadeOutAcabou);
+	}
+	function fadeOutAcabou() {
+	game.state.start("level1");	
+	}
+//clicarnotitle
+	function titleFoiClicado() {
+		fadeOut(fadeOutAcabo);
+	}
+	function fadeOutAcabo() {
+	game.state.start("menu");	
+	}
+	
+	
+}
---
+++ intro.js
@@ -0,0 +1,35 @@
+﻿function intro() {
+	
+
+	this.preload = function () {
+	game.load.image("intro", "intro.png");
+	game.load.image("title", "skip.png");
+	game.load.audio("flavio1", "flavio1.mp3");
+	};
+	
+	this.create = function () {
+		
+	game.add.image(0, 0, "intro");
+		
+
+	//configurartitle
+	title = game.add.image(320, 550, "title");
+	title.inputEnabled = true;
+	title.input.useHandCursor = true;
+	title.events.onInputDown.add(titleFoiClicado);
+	fadeIn();
+		};
+
+
+//clicarnotitle
+	function titleFoiClicado() {
+		fadeOut(fadeOutAcabou);
+	}
+	function fadeOutAcabou() {
+	flavio = game.add.audio("flavio1" , 2);
+	flavio.play();
+	game.state.start("level1");	
+	}
+	
+	
+}
